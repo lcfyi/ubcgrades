d3f56215a7b81f46109fbeab7fea7202e812796e
Leslie Cheng <leslie.cheng5@gmail.com>
2021-05-06 19:17:39 -0700

    Generated site.

---
+++ static/assets/js/main.js
@@ -0,0 +1,64 @@
+/*-----------------------------------
+* Global Variables
+*-----------------------------------*/
+
+let yearsession;
+let apiVersion;
+let campus = localStorage.getItem("campus");
+
+const YEARSESSIONS_UBCV = ['2020S', '2019W', '2019S', '2018W', '2018S', '2017W', '2017S', '2016W', '2016S', '2015W', '2015S', '2014W', '2014S', '2013W', '2013S', '2012W', '2012S', '2011W', '2011S', '2010W', '2010S', '2009W', '2009S', '2008W', '2008S', '2007W', '2007S', '2006W', '2006S', '2005W', '2005S', '2004W', '2004S', '2003W', '2003S', '2002W', '2002S', '2001W', '2001S', '2000W', '2000S', '1999W', '1999S', '1998W', '1998S', '1997W', '1997S', '1996W', '1996S'];
+const YEARSESSIONS_UBCO = YEARSESSIONS_UBCV.slice(0, 13); // Update this for new yearsessions
+const YEARSESSIONS_RECENT = YEARSESSIONS_UBCV.slice(0, 11); // fixed
+const HEADMATTER_V1_KEYS = ["average", "stdev", "high", "low", "pass", "fail", "enrolled", "withdrew", "audit", "other"];
+const HEADMATTER_V2_KEYS = ["average", "stdev", "high", "low", "enrolled"];
+const GRADES_V1 = ["0-9%", "10-19%", "20-29%", "30-39%", "40-49%", "<50%", "50-54%", "55-59%", "60-63%", "64-67%", "68-71%", "72-75%", "76-79%", "80-84%", "85-89%", "90-100%"];
+const GRADES_V2 = GRADES_V1.slice(5);
+const API_HOST_URL = "https://ubcgrades.com";
+
+class YearSession {
+    constructor(yearsession) {
+        this.year = parseInt(yearsession.substring(0, 4));
+        this.session = yearsession.substring(4);
+    }
+
+    toString() {
+        return this.year + this.session;
+    }
+}
+
+
+/*-----------------------------------
+* Campus Selector
+*-----------------------------------*/
+// Populate local storage with the desired campus if not already existing
+if (localStorage.getItem("campus") == null) $('#campusModal').modal('show');
+
+
+
+/*-----------------------------------
+* Display Errors
+*-----------------------------------*/
+// Use a counter to generate and collapse multiple alerts
+let messageCounter = 0;
+
+function displayError(message) {
+    displayMessage(message, "Error.", "danger");
+}
+
+function displayInfo(message) {
+    displayMessage(message, "Info.", "info");
+}
+
+function displayWarning(message) {
+    displayMessage(message, "Warning.", "warning");
+}
+
+function displayMessage(message, title, type) {
+    let messageId = `error-${messageCounter}`;
+    $("#notification").append(`<div class="alert alert-${type} mt-3" id="${messageId}"><strong>${title}</strong> ${message}</div>`);
+    $(`#${messageId}`).fadeTo(1500, 500).slideUp(500, function () {
+        $(`#${messageId}`).slideUp(1000);
+        $(`#${messageId}`).remove();
+    });
+    messageCounter++;
+}
---
+++ static/assets/js/statistics-by-course.js
@@ -0,0 +1,440 @@
+'use strict';
+
+$(function () {
+
+    /**
+     * Similar to the state machine for viewing grades, but much simpler
+     * since we only care about two variables.
+     */
+    function StateHandler() {
+        let _subject;
+        let _course;
+        let _synchronizing = false;
+
+        let updateOrder = [
+            "subject",
+            "course",
+        ]
+
+        let constructHash = () => {
+            return `${_subject}-${_course}`;
+        };
+
+        this.updateSubject = async (subject, dropdown = true) => {
+            _subject = subject;
+            if (!_synchronizing) {
+                // Reset the course so we get the dropdown
+                _course = null;
+            }
+            if (dropdown) {
+                updateCourseDropdown(subject, _course);
+            }
+        };
+
+        this.updateCourse = async (course, dropdown = true) => {
+            _course = course;
+            this.updateState();
+            if (dropdown) {
+                $('#sc-dropdown-form').submit();
+            }
+        };
+
+        /**
+         * Last stage of the state machine that consolidates all
+         * the substates and pushes the state to the browser history stack.
+         */
+        this.updateState = () => {
+            _synchronizing = false;
+            window.location.hash = constructHash();
+            $("#sc-copy-url-input").val(document.location.href);
+        };
+
+        /**
+         * Resets the state machine and updates the browser history.
+         */
+        this.resetState = () => {
+            _synchronizing = false;
+            let data = [null, null];
+            [_subject, _course] = data;
+            for (let i = 0; i < data.length; i++) {
+                $(`#sc-drop-${updateOrder[i]}`).val(data[i]).trigger("change");
+            }
+            window.history.replaceState(undefined, undefined, " ");
+        };
+
+        /**
+         * Synchronizes the state machine with the URL fragment.
+         */
+        this.synchronizeState = () => {
+            _synchronizing = true;
+            let fragments = window.location.hash.substring(1).split("-");
+            if (fragments.length === 2) {
+                [_subject, _course] = fragments;
+                $("#sc-drop-subject").select2("trigger", "select", {
+                    data: {
+                        id: _subject,
+                    }
+                });
+            } else {
+                this.resetState();
+            }
+        };
+
+        this.init = () => {
+            if (window.location.hash) {
+                this.synchronizeState();
+            }
+        };
+
+        window.addEventListener("hashchange", () => {
+            if (constructHash() !== window.location.hash.substring(1)) {
+                this.synchronizeState();
+            }
+        });
+
+        /**
+         * Share URL handlers below.
+         */
+        $("#sc-copy-url-form").on("submit", () => {
+            try {
+                navigator.clipboard.writeText(document.location.href);
+                displayInfo("Copied!");
+            } catch (e) {
+                displayError("Failed to copy, try manually.");
+            }
+            return false;
+        });
+
+        $("#sc-copy-url-input").on("click", (e) => {
+            e.target.setSelectionRange(0, e.target.value.length)
+        });
+    }
+
+    let stateHandler = new StateHandler();
+    /*-----------------------------------
+    * Campus Selector and Dropdowns
+    *-----------------------------------*/
+
+    // Campus modal handler
+    $('#campusModal .modal-body button').on('click', function (event) {
+        // Find the button that caused the modal to close and update the campus
+        let $button = $(event.target);
+        $(this).closest('.modal').one('hidden.bs.modal', function () {
+            localStorage.setItem("campus", $button.data('campus'));
+            campus = localStorage.getItem("campus");
+
+            populateSubjectDrop();
+        });
+    });
+
+    function populateSubjectDrop() {
+        // Restrict lookup to courses offered in the past 5 years
+        $.ajax({
+            url: `${API_HOST_URL}/api/v2/subjects/${campus}`,
+            type: "GET",
+            success: function (response) {
+                let data = response.map(item => ({
+                    'id': item['subject'],
+                    'text': `${item['subject']} - ${item['subject_title']}`
+                }));
+                $('#sc-drop-subject').prepend('<option></option>').select2({
+                    data: data,
+                    // // Auto adjust the dropdown
+                    dropdownAutoWidth: true,
+                    // // Display the ID instead of the value
+                    templateSelection: function (val) {
+                        return val.text === 'Subject' ? val.text : val.id; // Manual placeholder override as it has
+                    }
+                });
+                stateHandler.init();
+            },
+            error: function () {
+                displayError("Unable to connect to API");
+            }
+        });
+    }
+
+    $('#sc-drop-subject').select2().on("select2:select", function (e) {
+        stateHandler.updateSubject($(this).select2('data')[0]['id']);
+    });
+
+    $('#sc-drop-course').select2().on('select2:select', function(e) {
+        stateHandler.updateCourse($(this).select2('data')[0]['id']);
+    });
+
+    if (campus != null) {
+        // If the campus is already filled, automatically load the subjects
+        populateSubjectDrop();
+    }
+
+    function updateCourseDropdown(subject, previousCourse) {
+        $.ajax({
+            url: `${API_HOST_URL}/api/v2/courses/${campus}/${subject}`,
+            type: "GET",
+            success: function (response) {
+                let data = response.map(item => ({
+                    'id': `${item['course']}${item['detail']}`,
+                    'text': `${item['course']}${item['detail']} - ${item['course_title']}`
+                }));
+                let dropdown = $('#sc-drop-course');
+                dropdown.empty().prepend('<option selected=""></option>').select2({
+                    data: data,
+                    // Auto adjust the dropdown
+                    dropdownAutoWidth: true,
+                    // Display the ID instead of the value
+                    templateSelection: function (val) {
+                        return val.id;
+                    }
+                });
+                if (!previousCourse) {
+                    dropdown.select2("open");
+                } else {
+                    dropdown.select2("trigger", "select", {
+                        data: {
+                            id: previousCourse,
+                        }
+                    });
+                }
+            },
+            error: function () {
+                displayError("Unable to connect to API");
+            }
+        });
+    }
+
+
+    /*-----------------------------------
+    * Entry Points
+    *-----------------------------------*/
+
+    // Takes an input string and attempts to extract an array containing YEARSESSION, SUBJECT, COURSE, SECTION
+    function parseStatisticsByCourseID(inputStr) {
+        // Remove all non-character/numbers
+        let stripped = inputStr.replace(/(?![A-Za-z0-9]+)./gi, "");
+        // Split the result into four groups by regex. Index 0 contains the full match
+        let results = stripped.match(/([A-Z]+?(?=[0-9]))([0-9]{3}[A-Z]?)/i);
+
+        if (results == null || results.length !== 3) return false;
+
+        return results.slice(1, 3).map(str => str.toUpperCase());
+    }
+
+    // Entry via ID
+    $("#sc-id-form").on('submit', function () {
+        let idSplit = parseStatisticsByCourseID($('#sc-id-form input').val());
+        if (idSplit === false) {
+            displayError("Invalid ID. Check again.");
+        } else {
+            stateHandler.updateSubject(idSplit[0], false);
+            stateHandler.updateCourse(idSplit[1], false);
+            getCourseStatistics('#sc-id-submit', idSplit[0], idSplit[1]);
+        }
+        return false;
+    });
+
+    // Entry via dropdowns
+    $("#sc-dropdown-form").on('submit', function () {
+        // Selectively show row based on desired year
+        getCourseStatistics('#sc-dropdown-submit', $('#sc-drop-subject').val(), $('#sc-drop-course').val());
+
+        return false; // Don't refresh the page.
+    });
+
+    function displayContentContainer() {
+        let $content = $('#content-container')
+        if ($content.hasClass("collapse")) {
+            setTimeout(function () {
+                $content.collapse();
+                // Scroll user to the container
+                // $([document.documentElement, document.body]).animate({
+                //     scrollTop: $content.offset().top - 25
+                // }, 500);
+            }, 250);
+        }
+    }
+
+    function getCourseStatistics(button, subject, course) {
+        let $button = $(button);
+        let loadingText = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>';
+        if ($button.html() !== loadingText) {
+            // Store original text
+            $button.data('original-text', $button.html());
+            // Set disabled state for button
+            $button.addClass('disabled');
+            // Set loading text
+            $button.html(loadingText);
+        }
+
+        // Retrieve the data
+        $.ajax({
+            url: `${API_HOST_URL}/api/v2/course-statistics/${campus}//${subject}/${course}`,
+            type: "GET",
+            success: function (response) {
+                updateCourseHeadmatter(response);
+
+                // Retrieve historical averages
+                $.ajax({
+                    url: `${API_HOST_URL}/api/v2/course-statistics/average-history/${campus}/${subject}/${course}`,
+                    type: "GET",
+                    success: function (response) {
+                        updateHistoricalAveragesChart(response);
+                    }
+                });
+
+                // Retrieve professors
+                $.ajax({
+                    url: `${API_HOST_URL}/api/v2/course-statistics/teaching-team/${campus}/${subject}/${course}`,
+                    type: "GET",
+                    success: function (response) {
+                        updateTeachingTeamTable(response);
+                        $('#teaching-team h2').text(`${subject} ${course} Teaching Team`);
+                    }
+                });
+
+                displayContentContainer();
+            },
+            error: function (response) {
+                if (response.status === 404) displayError('Invalid ID, or ID does not exist.');
+                else displayError("Unable to connect to API");
+            },
+            complete: function () {
+                $button.html($button.data('original-text'));
+                $button.removeClass('disabled');
+            }
+        });
+    }
+
+    function updateCourseHeadmatter(data) {
+        // Update the card header
+        $('#sc-row .card-header h3').text(`${data['campus']}  ${data['subject']} ${data['course']}${data['detail']}`);
+        $('#sc-row .card-header h2').text(data['course_title']);
+        // Update the headmatter
+        let elements_h2 = $('#sc-headmatter h2');
+        $(elements_h2[0]).text(parseFloat(data['average']).toFixed(2));
+        $(elements_h2[1]).text(parseFloat(data['average_past_5_yrs'].toFixed(2)));
+        $(elements_h2[2]).text(parseFloat(data['max_course_avg'].toFixed(2)));
+        $(elements_h2[3]).text(parseFloat(data['min_course_avg'].toFixed(2)));
+
+        // // Update the chart
+        // $('#chart-grades-toggles').show();
+    }
+
+    let historicalAveragesChart;
+
+    function updateHistoricalAveragesChart(apiResponseData) {
+        let avgHistData = apiResponseData['yearsessions'];
+        Object.entries(avgHistData).forEach(function(ele) {
+            const [key, value] = ele;
+            if (value === null) {
+                delete avgHistData[key];
+            }
+        });
+        let chartValues = Object.values(avgHistData);
+
+        // Update the 5 Years/All Years switcher
+        let recentValues = [], recentLabels = [];
+        YEARSESSIONS_RECENT.slice().reverse().forEach(function(yearsession) {
+            if (yearsession in avgHistData) {
+                recentValues.push(avgHistData[yearsession]);
+                recentLabels.push(yearsession);
+            }
+        })
+
+        $('#five-years-toggle').on('click', function () {
+            historicalAveragesChart.data.labels = recentLabels;
+            historicalAveragesChart.data.datasets[0].data = recentValues;
+            historicalAveragesChart.update();
+        });
+
+        $('#all-years-toggle').on('click', function () {
+            historicalAveragesChart.data.labels = Object.keys(avgHistData)
+            historicalAveragesChart.data.datasets[0].data = chartValues
+            historicalAveragesChart.update();
+        });
+        
+        let $chart = $('#chart-course-avg-hist');
+
+        if (historicalAveragesChart) historicalAveragesChart.destroy();
+        historicalAveragesChart = new Chart($chart, {
+            type: 'line',
+            options: {
+                scales: {
+                    yAxes: [{
+                        gridLines: {
+                            lineWidth: 1,
+                            color: Charts.colors.gray[900],
+                            zeroLineColor: Charts.colors.gray[900]
+                        }
+                    }]
+                },
+                tooltips: {
+                    callbacks: {
+                        label: function (item, data) {
+                            let label = data.datasets[item.datasetIndex].label || '';
+                            let content = '';
+                            if (data.datasets.length > 1) {
+                                content += `<span class="popover-body-label mr-auto">${label}</span>`;
+                            }
+
+                            // Distinguish between entries with 0 values and no data
+                            if (chartValues[item.index] !== '') {
+                                content += `<span class="popover-body-value">${parseFloat(item.yLabel).toFixed(2)}</span>`;
+                            } else {
+                                content += `<span class="popover-body-value">No Data</span>`;
+                            }
+                            return content;
+                        }
+                    }
+                }
+            },
+            data: {
+                labels: Object.keys(avgHistData),
+                datasets: [{
+                    pointRadius: 5,
+                    data: chartValues
+                }]
+            }
+        });
+        // Save to jQuery object
+
+        $chart.data('chart', historicalAveragesChart);
+    }
+
+    // Init data table
+    let teachingTeamTable = $('#teaching-team table').DataTable({
+        columns: [
+            { title: "Instructor or TA" , className: 'font-weight-bold'},
+            { title: "YearSessions Active" },
+        ],
+        keys: !0,
+        select: {
+            style: "multi"
+        },
+        language: {
+            paginate: {
+                previous: "<i class='fas fa-angle-left'>",
+                next: "<i class='fas fa-angle-right'>"
+            }
+        },
+        style_cell: {
+            'whiteSpace': 'normal',
+            'height': 'auto',
+        },
+    });
+
+    function updateTeachingTeamTable(apiResp) {
+        // Parse the API response to map an instructor/TA to yearsessions they were active
+        // Format needs to be array of arrays for datatables
+        let teachingTeam = [];
+        apiResp.forEach(function(ele) {
+            if (ele['name'] === '') return;
+            // To string and push to datatable
+            teachingTeam.push([ele['name'], Object.keys(ele['yearsessions']).join(', ')])
+        });
+
+        // Update the table
+        teachingTeamTable.clear();
+        teachingTeamTable.rows.add(teachingTeam);
+        teachingTeamTable.draw();
+    }
+});
