4577719ee3ccab45506123ae6d2a04fdaa23fa74
Leslie Cheng <leslie.cheng5@gmail.com>
2021-05-06 19:29:18 -0700

    Add base url to work with GH pages.

--- freeze.py
+++ freeze.py
@@ -5,6 +5,7 @@ from app import create_app
 
 if __name__ == "__main__":
     Config.FREEZER_DESTINATION = "../build"
+    Config.FREEZER_BASE_URL = "https://lcfyi.github.io/ubcgrades/"
     app, _ = create_app(Config)
     freezer = Freezer(app)
     freezer.freeze()
